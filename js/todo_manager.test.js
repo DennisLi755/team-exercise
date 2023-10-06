9e5b74f2b3ccab8726d44021ffffb9870dcae3cc
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-06 22:05:52 +0200

    passed the initial test

--- js/todo_manager.test.js
+++ js/todo_manager.test.js
@@ -3,6 +3,6 @@ const {addItem,toDoArray} = require("./main");
 describe("test adding items", () => {
     test("addItem('washthedog') === add item washthedog to todo list",() => {
         addItem("washthedog")
-        expect(toDoArray[0]).tobe("washthedog");
+        expect(toDoArray).toContain("washthedog");
     })
 })
