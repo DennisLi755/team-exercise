a86d5952e90274fd51a5512411059e4603b0f646
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-06 22:11:28 +0200

    changed first test

--- js/todo_manager.test.js
+++ js/todo_manager.test.js
@@ -1,8 +1,14 @@
 const {addItem,toDoArray} = require("./main");
 
-describe("test adding items", () => {
+// describe("test adding items", () => {
+//     test("addItem('washthedog') === add item washthedog to todo list",() => {
+//         addItem("washthedog")
+//         expect(toDoArray).toContain("washthedog");
+//     })
+// })
+describe("test todolist", () => {
     test("addItem('washthedog') === add item washthedog to todo list",() => {
         addItem("washthedog")
-        expect(toDoArray).toContain("washthedog");
+        expect(toDoArray).toContain({task:"washthedog",completed:false});
     })
 })
