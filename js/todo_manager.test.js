346668ee3098d6816912c9ced1c72893441476e5
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-06 21:48:11 +0200

    inital test

---
+++ js/todo_manager.test.js
@@ -0,0 +1,8 @@
+const {addItem,toDoArray} = require("./main");
+
+describe("test adding items", () => {
+    test("addItem('washthedog') === add item washthedog to todo list",() => {
+        addItem("washthedog")
+        expect(toDoArray[0]).tobe("washthedog");
+    })
+}
