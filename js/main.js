409a17e30d0bee9133cfd78bfda02d826fcf6c69
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-06 22:44:05 +0200

    3rd sucessfull test (:

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 let toDoArray = [];
 
-function addItem(str){
-    return toDoArray.push({task:str, completed:false})
+function addItem(str) {
+  return toDoArray.push({ task: str, completed: false });
 }
-function markComplete(str){
-    for(i=0;i<toDoArray.length;i++){
-        if(toDoArray[i].task===str){
-            toDoArray[i].completed = true
-        }
+function markComplete(str) {
+  for (i = 0; i < toDoArray.length; i++) {
+    if (toDoArray[i].task === str) {
+      toDoArray[i].completed = true;
     }
+  }
+}
+function deleteItem(str) {
+  let toBeRemoved = null;
+  for (i = 0; i < toDoArray.length; i++) {
+    if (toDoArray[i].task === str) {
+      toBeRemoved = i;
+    }
+  }
+  toDoArray.splice(toBeRemoved, 1);
 }
 
-module.exports = {addItem,toDoArray,markComplete}
+module.exports = { addItem, toDoArray, markComplete, deleteItem };
--- js/todo_manager.test.js
+++ js/todo_manager.test.js
@@ -1,4 +1,4 @@
-const {addItem,toDoArray,markComplete} = require("./main");
+const {addItem,toDoArray,markComplete,deleteItem} = require("./main");
 
 describe("test add item", () => {
     test("addItem('washthedog') === add item washthedog to todo list",() => {
@@ -11,5 +11,12 @@ describe("test completor", () => {
     test("markComplete('washthedog') === mark washthedog to completed:true",() => {
         markComplete("washthedog")
         expect(toDoArray).toContainEqual({task:"washthedog",completed:true});
+    })
+})
+describe("test deletor", () => {
+    test("deleteItem('washthedog') === delete washthedog from list",() => {
+        //addItem("washthedog")
+        deleteItem("washthedog")
+        expect(toDoArray).not.toContainEqual({task:"washthedog",completed:true});
     })
 })
